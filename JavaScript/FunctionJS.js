5f48df01065152d3c77b2293a7a957c3e9bb4d06
THIVINKANTH <thivinkanth@gmail.com>
2022-12-22 16:49:38 +0530

    js Array

---
+++ JavaScript/FunctionJS.js
@@ -0,0 +1,18 @@
+function First()
+{
+    alert("Welcome to Java Script Function normal process")
+}
+
+function details(name,age)
+{
+    alert("my name is" +name+"my age is "+age)
+}
+
+var process=()=>
+{
+    document.writeln("This is fat arrow function using for java script")
+}
+const calculation=(a,b)=>
+{
+    return a*b;
+}
